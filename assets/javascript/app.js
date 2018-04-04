d4600e57139a947149f600b9ec806b6254a6ea4e
m-rodriguez7 <michael.rodriguez7@icloud.com>
2018-04-04 13:38:28 -0700

    added a few lines to the app.js file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,27 @@
-var p5characters = ["Morgana", "Rem Amiyama", "Ann Takamaki", "Ryuji Sakamoto", "Yusuke Hanamura", "Makoto Niijima", "Futaba Sakura", "Haru Okumura"]
+var topic = ["fill", "this", "array", "with", "anything"];
+
+// need to create buttons for everything in this array
+
+for (var i=0; i < topic.length; i++) {
+	newButton = $("<button>");
+	newButton.attr("dataTitle", topic[i]);
+	newButton.attr("class", "button");
+	newButton.text(topic[i]);
+	$("#gifButtons").append(newButton);
+};
+
+$("$addButton").on("click, function () {
+	newButton = $("<button>");
+	newButton.attr("dataTitle", $("#searchInput").val());
+	newButton.attr("class", "button");
+	newButton.text($("#searchInput").val());
+	$("#gifButtons").append(newButton);
+})
+
+
+$(".button").on("click", function () { // using a button class instead of all buttons
+	// when you click a button
+	// the page should fill with still images from the giphy api
+	// these still images should be based on a search from the button
+})
+
