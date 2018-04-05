8a5a39d98ec57458bd238ec48a13b44415295ddf
m-rodriguez7 <michael.rodriguez7@icloud.com>
2018-04-04 18:27:26 -0700

    more js edits

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -20,8 +20,24 @@ $("$addButton").on("click, function () {
 
 
 $(".button").on("click", function () { // using a button class instead of all buttons
-	// when you click a button
 	// the page should fill with still images from the giphy api
 	// these still images should be based on a search from the button
-})
+	// every gif on the page should load with data embedded in the giv div. the data should be a url for the 'animated' gif, as well as a url for the "still" shot.
+	// when the gif is clicked, the src for the gif should switch from the still shot to animated shot, and vice versa
+	// the gif should start with the "still" shot as its source.
+	// the still shot has essentially the same url as the actual gif, with a "_s" at the end of it "yaddayadda_s.gif" would be a still shot
+	$.ajax({
+		url: "" //insert giphy api url here
+		method: "GET"
+	}).then(function(response){
+		var results = response.data // this is an array to loop through to pull gifs from!
+		for (var i=0; i<results.length; i++) {
+			newGif = $("<img>");
+			newGif.attr("dataAnimate", results[i].images.downsized.url);
+			// add attr for dataStill
+			// set src to data still
+			// add attr for current state, still or animated
+		};
+	})
+});
 
